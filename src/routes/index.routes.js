c438bddea0747e684aa2b196d5c28a4dd7c714c1
Agustin Grigaliunas <agustingrigaliunas@gmail.com>
2022-02-19 20:20:30 -0300

    task update

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,26 +1,44 @@
 import { Router } from "express";
-import Task from '../models/Task';
+import Task from "../models/Task";
 
 const router = Router();
 
 router.get("/", async (req, res) => {
-  
   const taskArray = await Task.find().lean();
-  
-  res.render("index", { tasks: taskArray});
+  res.render("index", { tasks: taskArray });
 });
 
 router.get("/about", (req, res) => {
   res.send("about");
 });
 
-router.post("/tasks/add", async (req, res) =>{
+router.get("/edit/:id", async (req, res) => {
+  try { 
+    const taskToEdit = await Task.findById(req.params.id).lean();
+    res.render("edit", {task: taskToEdit});
+  }
+  catch (e) {
+    console.log(e);
+  }
+});
+
+
+router.post("/tasks/add", async (req, res) => {
   try {
-    const task = Task(req.body);
-    await task.save();
+    const newTask = Task(req.body);
+    await newTask.save();
     res.redirect("/");
   } catch (e) {
-      console.log(e);
+    console.log(e);
+  }
+});
+
+router.post("/edit/:id", async (req, res) => {
+  try {
+    await Task.findByIdAndUpdate(req.params.id, req.body);
+    res.redirect("/");
+  } catch (e) {
+    console.log(e);
   }
 });
 
