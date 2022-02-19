bd7bdc32aba5381ecf15c51bb19406803c90d9a6
Agustin Grigaliunas <agustingrigaliunas@gmail.com>
2022-02-19 18:07:59 -0300

    listed tasks

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,18 +1,23 @@
 import { Router } from "express";
+import Task from '../models/Task';
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.render("index");
+router.get("/", async (req, res) => {
+  
+  const taskArray = await Task.find().lean();
+  
+  res.render("index", { tasks: taskArray});
 });
 
 router.get("/about", (req, res) => {
   res.send("about");
 });
 
-router.post("/tasks/add", (req, res) =>{
-  const task = req.body;
-  res.send(task);
+router.post("/tasks/add", async (req, res) =>{
+  const task = Task(req.body);
+  await task.save();
+  res.redirect("/");
 });
 
 export default router;
