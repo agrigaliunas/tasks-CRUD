f993b0ade0ae04fd5c33faba023f0692c1976bb8
Agustin Grigaliunas <agustingrigaliunas@gmail.com>
2022-02-19 17:23:01 -0300

    db connected fixed

--- src/database.js
+++ src/database.js
@@ -1,13 +1,10 @@
-import {connect} from "mongoose"
+import { connect } from "mongoose";
 
 (async () => {
-
-try{
+  try {
     const db = await connect("mongodb://localhost/crud-db");
     console.log("DB connected to: ", db.connection.name);
-}
-catch (e){ 
+  } catch (e) {
     console.log(e);
-}
-
+  }
 })();
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import app from "./app";
-
+import "./database";
 app.listen(3000);
 console.log("Server is up on port ", 3000);
