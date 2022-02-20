119d07c0a553f494d972a1c60f8bd8b65c6fa504
Agustin Grigaliunas <agustingrigaliunas@gmail.com>
2022-02-19 21:01:05 -0300

    controllers implemented

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -1,4 +1,50 @@
+import Task from "../models/Task";
+
 export const renderTasks = async (req, res) => {
-    const taskArray = await Task.find().lean();
-    res.render("index", { tasks: taskArray });
+  const taskArray = await Task.find().lean();
+  res.render("index", { tasks: taskArray });
+};
+
+export const createTasks = async (req, res) => {
+  try {
+    const newTask = Task(req.body);
+    await newTask.save();
+    res.redirect("/");
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const renderEditTasks = async (req, res) => {
+  try {
+    const taskToEdit = await Task.findById(req.params.id).lean();
+    res.render("edit", { task: taskToEdit });
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const deleteTasks = async (req, res) => {
+  try {
+    await Task.findByIdAndDelete(req.params.id);
+    res.redirect("/");
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const editTasks = async (req, res) => {
+  try {
+    await Task.findByIdAndUpdate(req.params.id, req.body);
+    res.redirect("/");
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const toggleDoneTasks = async (req, res) => {
+  const taskToggleDone = await Task.findById(req.params.id);
+  taskToggleDone.done = !taskToggleDone.done;
+  await taskToggleDone.save();
+  res.redirect("/");
 };
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,57 +1,25 @@
 import { Router } from "express";
-import Task from "../models/Task";
-import { renderTasks } from "../controllers/tasks.controller";
+import {
+  renderTasks,
+  createTasks,
+  renderEditTasks,
+  deleteTasks,
+  editTasks,
+  toggleDoneTasks,
+} from "../controllers/tasks.controller";
 
 const router = Router();
 
 router.get("/", renderTasks);
 
-router.get("/about", (req, res) => {
-  res.send("about");
-});
+router.get("/edit/:id", renderEditTasks);
 
-router.get("/edit/:id", async (req, res) => {
-  try {
-    const taskToEdit = await Task.findById(req.params.id).lean();
-    res.render("edit", { task: taskToEdit });
-  } catch (e) {
-    console.log(e);
-  }
-});
+router.get("/delete/:id", deleteTasks);
 
-router.get("/delete/:id", async (req, res) => {
-  try {
-    await Task.findByIdAndDelete(req.params.id);
-    res.redirect("/");
-  } catch (e) {
-    console.log(e);
-  }
-});
+router.get("/toggleDone/:id", toggleDoneTasks);
 
-router.get("/toggleDone/:id", async (req, res) => {
-  const taskToggleDone = await Task.findById(req.params.id);
-  taskToggleDone.done = !taskToggleDone.done;
-  await taskToggleDone.save();
-  res.redirect("/");
-});
+router.post("/tasks/add", createTasks);
 
-router.post("/tasks/add", async (req, res) => {
-  try {
-    const newTask = Task(req.body);
-    await newTask.save();
-    res.redirect("/");
-  } catch (e) {
-    console.log(e);
-  }
-});
-
-router.post("/edit/:id", async (req, res) => {
-  try {
-    await Task.findByIdAndUpdate(req.params.id, req.body);
-    res.redirect("/");
-  } catch (e) {
-    console.log(e);
-  }
-});
+router.post("/edit/:id", editTasks);
 
 export default router;
